02283036f91786a49cd9a8c6823dcb7fbd4509dd
x <y>
2020-04-14 18:10:50 +0700

    Fixed closing brace completion error. If a closing brace was typed, and  caret was at the end of an editor, then an exception was thrown.
    * Added auto-completion of ; character.
    * Some refactoring in changed file.

--- MPL/Commands/BraceCompletionCommandHandler.cs
+++ MPL/Commands/BraceCompletionCommandHandler.cs
@@ -8,26 +8,24 @@ using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Shell;
 using System.Runtime.InteropServices;
 using MPL.AST;
+using System.Runtime.CompilerServices;
 
 namespace MPL.Commands {
   class BraceCompletionCommandHandler : IOleCommandTarget {
-    private char typedChar;
-    private SnapshotPoint point;
-    private readonly Dictionary<char, char> bracePairs;
+    private static readonly Dictionary<char, char> bracePairs = new Dictionary<char, char> {
+      ['{'] = '}',
+      ['['] = ']',
+      ['('] = ')',
+      [':'] = ';'  // We treat label start (:) and label end (;) as a brace pair.
+                   // So now, we have auto-completion of closing ; character after label start.
+    };
+
     private IOleCommandTarget _NextCommandTarget;
-    protected readonly IVsTextView VsTextView;
     protected readonly IWpfTextView TextView;
 
     public BraceCompletionCommandHandler(IVsTextView vsTextView, IWpfTextView textView) {
-      VsTextView = vsTextView;
       TextView = textView;
-      bracePairs = new Dictionary<char, char> {
-        ['{'] = '}',
-        ['['] = ']',
-        ['('] = ')'
-      };
-
-      VsTextView.AddCommandFilter(this, out _NextCommandTarget);
+      vsTextView.AddCommandFilter(this, out _NextCommandTarget);
     }
 
     public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText) {
@@ -37,34 +35,76 @@ namespace MPL.Commands {
 
     public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut) {
       ThreadHelper.ThrowIfNotOnUIThread();
-      if (pguidCmdGroup == VSConstants.VSStd2K && nCmdID == (uint)VSConstants.VSStd2KCmdID.TYPECHAR && MplPackage.Options.AutoBraceCompletion) {
-        typedChar = (char)(ushort)Marshal.GetObjectForNativeVariant(pvaIn);
-        point = TextView.Caret.Position.BufferPosition;
-        if (IsStringOrComment()) {
-          return _NextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
-        }
-      } else {
-        return _NextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
-      }
 
-      if (typedChar == '[' || typedChar == '{' || typedChar == '(') {
-        var nextCommand = _NextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
-        var caretPoint = TextView.Caret.Position.BufferPosition;
-        TextView.TextBuffer.Insert(TextView.Caret.Position.BufferPosition.Position, bracePairs[typedChar].ToString());
-        TextView.Caret.MoveTo(caretPoint.TranslateTo(TextView.TextSnapshot, PointTrackingMode.Negative));
-      } else if ((typedChar == ']' || typedChar == '}' || typedChar == ')') && TextView.Caret.Position.BufferPosition.GetChar() == typedChar) {
-        TextView.Caret.MoveToNextCaretPosition();
-      } else {
-        return _NextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+      if (!IsBraceCompletionNeeded(ref pguidCmdGroup, nCmdID))
+        goto noCompletionNeeded;
+
+      var typedChar = (char)(ushort)Marshal.GetObjectForNativeVariant(pvaIn);
+
+      if (IsOpeningBrace(typedChar))
+        return HandleOpeningBrace(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut, typedChar);
+
+      if (IsClosingBrace(typedChar) && IsEqualToNextCharacter(typedChar))
+        return HandleClosingBrace();
+
+    noCompletionNeeded:
+      return _NextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+    }
+
+    private bool IsEqualToNextCharacter(char typedChar) =>
+      IsEndOfInput()
+      ? false
+      : TextView.Caret.Position.BufferPosition.GetChar() == typedChar;
+
+    // TODO: Maybe vs api have some tools for that.
+    // FIXME: Get rid of the exception.
+    private bool IsEndOfInput() {
+      try {
+        _ = TextView.Caret.Position.BufferPosition.GetChar();
+        return false;
       }
+      catch (Exception) {
+        return true;
+      }
+    }
+
+    private int HandleClosingBrace() {
+      TextView.Caret.MoveToNextCaretPosition();
+      return VSConstants.S_OK;
+    }
+
+    private int HandleOpeningBrace(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut, char typedChar) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+
+      _ = _NextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+
+      var caretPoint = TextView.Caret.Position.BufferPosition;
+      TextView.TextBuffer.Insert(TextView.Caret.Position.BufferPosition.Position, bracePairs[typedChar].ToString());
+      TextView.Caret.MoveTo(caretPoint.TranslateTo(TextView.TextSnapshot, PointTrackingMode.Negative));
 
       return VSConstants.S_OK;
     }
 
+    private bool IsBraceCompletionNeeded(ref Guid pguidCmdGroup, uint nCmdID) {
+      ThreadHelper.ThrowIfNotOnUIThread();
+
+      return
+        MplPackage.Options.AutoBraceCompletion
+        && IsActiveInput(ref pguidCmdGroup, nCmdID)
+        && !IsStringOrComment();
+    }
+
+    private static bool IsActiveInput(ref Guid pguidCmdGroup, uint nCmdID)
+      => pguidCmdGroup == VSConstants.VSStd2K && nCmdID == (uint)VSConstants.VSStd2KCmdID.TYPECHAR;
+
+    private static bool IsOpeningBrace(char a) => bracePairs.ContainsKey(a);
+    private static bool IsClosingBrace(char a) => bracePairs.ContainsValue(a);
+
     private bool IsStringOrComment() {
       TreeBuilder.Node root = AST.AST.GetASTRoot();
-      bool notStringOrComment = true;
-      bool notReached = true;
+      var notStringOrComment = true;
+      var notReached = true;
+      var point = TextView.Caret.Position.BufferPosition;
 
       void Traverse(TreeBuilder.Node node) {
         if (node.children == null) {
@@ -88,6 +128,6 @@ namespace MPL.Commands {
       Traverse(root);
 
       return !notStringOrComment;
-    }  
+    }
   }
 }
